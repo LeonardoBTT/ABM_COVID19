933ec00f387812c55d9249fbf96392c9b6b150ae
Leonardo Bittencourt <leonardo1silva2@gmail.com>
2022-04-08 14:20:12 -0300

    08-04

--- models/experiments/init.bat
+++ models/experiments/init.bat
@@ -8,34 +8,34 @@ timeout /t 300
 start cmd /k gama-headless.bat impor_testagem_100.xml simulacoes/impor_testagem_100
 timeout /t 300
 
-cd '../Importancia das diferentes vacinas'
+::cd ../Importancia das diferentes vacinas
 
-start cmd /k gama-headless.bat dif_vacinas_5038.xml simulacoes/dif_vacinas_5038 
-timeout /t 300
-start cmd /k gama-headless.bat dif_vacinas_7040.xml simulacoes/dif_vacinas_7040 
-timeout /t 300
-start cmd /k gama-headless.bat dif_vacinas_9140.xml simulacoes/dif_vacinas_9140
-timeout /t 300
-start cmd /k gama-headless.bat dif_vacinas_9450.xml simulacoes/dif_vacinas_9450 
-timeout /t 300
-start cmd /k gama-headless.bat dif_vacinas_9500.xml simulacoes/dif_vacinas_9500 
-timeout /t 300
+::start cmd /k gama-headless.bat dif_vacinas_5038.xml simulacoes/dif_vacinas_5038 
+::timeout /t 300
+::start cmd /k gama-headless.bat dif_vacinas_7040.xml simulacoes/dif_vacinas_7040 
+::timeout /t 300
+::start cmd /k gama-headless.bat dif_vacinas_9140.xml simulacoes/dif_vacinas_9140
+::timeout /t 300
+::start cmd /k gama-headless.bat dif_vacinas_9450.xml simulacoes/dif_vacinas_9450 
+::timeout /t 300
+::start cmd /k gama-headless.bat dif_vacinas_9500.xml simulacoes/dif_vacinas_9500 
+::timeout /t 300
 
-cd '../Importancia das vacinas'
+::cd ../Importancia das vacinas
 
-start cmd /k gama-headless.bat impor_vacinas_25.xml simulacoes/impor_vacinas_25 
-timeout /t 300
-start cmd /k gama-headless.bat impor_vacinas_50.xml simulacoes/impor_vacinas_50 
-timeout /t 300
-start cmd /k gama-headless.bat impor_vacinas_75.xml simulacoes/impor_vacinas_75 
-timeout /t 300
-start cmd /k gama-headless.bat impor_vacinas_100.xml simulacoes/impor_vacinas_100 
-timeout /t 300
+::start cmd /k gama-headless.bat impor_vacinas_25.xml simulacoes/impor_vacinas_25 
+::timeout /t 300
+::start cmd /k gama-headless.bat impor_vacinas_50.xml simulacoes/impor_vacinas_50 
+::timeout /t 300
+::start cmd /k gama-headless.bat impor_vacinas_75.xml simulacoes/impor_vacinas_75 
+::timeout /t 300
+::start cmd /k gama-headless.bat impor_vacinas_100.xml simulacoes/impor_vacinas_100 
+::timeout /t 300
 
-cd '../Importancia de respeitar as normas sanitarias'
+cd ../Importancia de respeitar as normas sanitarias
 
-start cmd /k gama-headless.bat impor_normas_25.xml simulacoes/impor_normas_25
-timeout /t 300
+::start cmd /k gama-headless.bat impor_normas_25.xml simulacoes/impor_normas_25
+::timeout /t 300
 start cmd /k gama-headless.bat impor_normas_50.xml simulacoes/impor_normas_50 
 timeout /t 300
 start cmd /k gama-headless.bat impor_normas_75.xml simulacoes/impor_normas_75
@@ -43,7 +43,7 @@ timeout /t 300
 start cmd /k gama-headless.bat impor_normas_100.xml simulacoes/impor_normas_100 
 timeout /t 300
 
-cd '../Importancia de respeitar as normas sanitarias após um evento inesperado'
+cd ../Importancia de respeitar as normas sanitarias após um evento inesperado
 
 start cmd /k gama-headless.bat apos_evento_25.xml simulacoes/apos_evento_25
 timeout /t 300
